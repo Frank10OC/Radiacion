10b42ccea2b62837ce09bcea6c6d357f280b5f6e
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2023-09-25 23:38:51 -0500

    Update cod.py

--- cod.py
+++ cod.py
@@ -13,9 +13,39 @@ if nombre:
 else:
     st.write("Por favor, ingresa tu nombre en el cuadro de texto arriba.")
 import math
-
 import streamlit as st
-import math
+# Título de la aplicación
+st.title("Cálculo de δ")
+
+# Entrada de valores
+n = st.number_input("Ingrese el valor de n:")
+
+# Función para calcular δ
+def calcular_delta(n):
+    delta = 23.45 * math.sin(0.9856 * n)
+    return delta
+
+# Botón para realizar el cálculo
+if st.button("Calcular δ"):
+    resultado = calcular_delta(n)
+    st.write(f"El valor de δ es: {resultado}")
+
+# Título de la aplicación
+st.title("Cálculo de d")
+
+# Entrada de valores
+n = st.number_input("Ingrese el valor de n:")
+dd = st.number_input("Ingrese el valor de đ:")
+
+# Función para calcular d
+def calcular_d(n, dd):
+    d = dd * (1 + 0.17 * math.sin(0.9856 * n))
+    return d
+
+# Botón para realizar el cálculo
+if st.button("Calcular d"):
+    resultado = calcular_d(n, dd)
+    st.write(f"El valor de d es: {resultado}")
 
 # Título de la aplicación
 st.title("Cálculo de RA")
@@ -35,7 +65,6 @@ def calcular_ra(phi, delta, omega, d, dd):
     
     RA = 889 * pow((dd / d),2) * (0.01745 * omega_rad * math.sin(phi_rad) * math.sin(delta_rad) + math.cos(phi_rad) * math.cos(delta_rad) * math.sin(omega_rad))
     return RA
-
 # Botón para realizar el cálculo
 if st.button("Calcular RA"):
     resultado = calcular_ra(phi, delta, omega, d, dd)
