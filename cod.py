1811046f4b01b32f2f3dc22b3b1bae698374eabf
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2023-10-01 23:48:35 -0500

    Update cod.py

--- cod.py
+++ cod.py
@@ -1,17 +1,4 @@
-import streamlit as st
 
-# Título de la aplicación
-st.title("Aplicación de Saludo")
-
-# Entrada del usuario
-nombre = st.text_input("Por favor, ingresa tu nombre")
-
-# Verificación si se ingresó un nombre
-if nombre:
-    # Salida personalizada
-    st.write(f"Hola, {nombre}! Bienvenido a mi aplicación de Streamlit.")
-else:
-    st.write("Por favor, ingresa tu nombre en el cuadro de texto arriba.")
 import math
 import streamlit as st
 def grados_a_radianes(grados):
