ec5fa30009227e59fa33e134b993037ce3f4d504
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2023-10-03 13:04:14 -0500

    Update cod.py

--- cod.py
+++ cod.py
@@ -29,9 +29,6 @@ def calcular_radiacion_solar_para_dataframe(df, columna_N):
 # Calcular la radiación solar para la columna 'N' utilizando la función
 df = calcular_radiacion_solar_para_dataframe(dn, 'Enero')
 st.dataframe(df)
-da = calcular_radiacion_solar_para_dataframe(dn, 'Febrero')
-st.dataframe(da)
-
 
 
 
